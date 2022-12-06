032f494db65f0a95ba870f53bd18db3a3851bea9
luiznijah <luiznijah@gmail.com>
2022-12-05 21:07:05 -0300

    Modificações no exercicio1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,18 +2,25 @@
 // Utilizando o laço `while`, escreva um código que recebe um `prompt` perguntando se a pessoa deseja comer mais coxinhas. “S” deve representar “sim”, e “N” deve representar “não”. Crie também uma `let conta` para guardar o valor total.
 
 
-let coxinha = prompt("Você deseja comer mais coxinha sim ou não ?").toLocaleUpperCase()
-let total = 0
-let conta = 0
-let valor = 2.50
+let coxinha = prompt(`Você deseja comer mais coxinha ? digite 
+S- para sim 
+N- para não `).toLocaleUpperCase();
+
+let total = 0;
+
+let conta = 0;
+
+let valor = 2.50;
+
 while(coxinha=== "S"){
     conta =  conta + valor
     
     total++
-    coxinha = prompt("Você deseja comer mais coxinha sim ou não ?").toLocaleUpperCase()
+    coxinha = prompt(`Você deseja comer mais coxinha ? digite 
+    S- para sim 
+    N- para não `).toLocaleUpperCase()
 
-}
-console.log(`foram ${total} coxinhas e o total da conta é ${conta.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}`)
+};
+console.log(`foram ${total} coxinhas e o total da conta é ${conta.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}`);
 
 
-// Toda vez que a resposta for **sim**, o laço deve se repetir, e devemos adicionar R$2.50 ao valor total. Quando a resposta não for **sim**, devemos imprimir o valor total da conta.
